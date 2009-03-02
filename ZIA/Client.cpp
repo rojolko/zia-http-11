e3ed055ccce6e1dd7611e760e2b4aeb364d1c59e
rojolko <rojolko@503aef8a-0296-11de-802b-177316e4ac06>
2009-03-02 19:27:13 +0000

    Instanciation des clients ok.
    Stockage de l'instance dans une MAP et non une LISTE. (Vu avec Yoru, stoi l'vu).
    
    Destructeur de ConnectionManager qui Free la liste fait. (Pas testé, on ne quitte jamais le serveur proprement pour l'instant CTRL-C inside ...).
    
    Ajout de qques attributs pour stocker les infos Winsock dans la classe Client.
    
    Diverses micro corrections de forme (rien de ouf).

--- ZIA/Client.cpp
+++ ZIA/Client.cpp
@@ -1,9 +1,18 @@
 #include "Client.h"
 
-Client::Client(SOCKET)
+Client::Client(SOCKET sock, sockaddr srcInf, SOCKADDR_IN srcInfIn)
 {
+	this->_sock = sock;
+	this->_clientSrcInf = srcInf;
+	this->_clientSrcInfIn = srcInfIn;
 }
 
 Client::~Client(void)
 {
+
+}
+
+const char	*Client::getIp()
+{
+	return (inet_ntoa((in_addr)this->_clientSrcInfIn.sin_addr));
 }
