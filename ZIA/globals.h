a7d0bc9d9b9d4e9b0d4df432197a379bd9a3c7ac
rojolko <rojolko@503aef8a-0296-11de-802b-177316e4ac06>
2009-04-02 10:26:52 +0000

    Ajout des codes d'erreur dans globals.h

--- ZIA/globals.h
+++ ZIA/globals.h
@@ -1,6 +1,61 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
+typedef struct			s_statcde
+{
+	const unsigned int	cde;
+	const char			*str;
+}						t_statcde;
+
+t_statcde	t_SatusCode[]=
+{
+	{100, "Continue"},
+	{101, "Switching Protocols"},
+
+	{200, "OK"},
+	{201, "Created"},
+	{202, "Accepted"},
+	{203, "Non-Authoritative Information"},
+	{204, "No Content"},
+	{205, "Reset Content"},
+	{206, "Partial Content"},
+
+	{300, "Multiple Choices"},
+	{301, "Moved Permanently"},
+	{302, "Found"},
+	{303, "See Other"},
+	{304, "Not Modified"},
+	{305, "Use Proxy"},
+	{307, "Temporary Redirect"},
+	{400, "Bad Request"},
+	{401, "Unauthorized"},
+	{402, "Payment Required"},
+	{403, "Forbidden"},
+	{404, "Not Found"},
+	{405, "Method Not Allowed"},
+	{406, "Not Acceptable"},
+
+	{407, "HTTP Version not supported"},
+	{408, "Request Time-out"},
+	{409, "Conflict"},
+	{410, "Gone"},
+	{411, "Length Required"},
+	{412, "Precondition Failed"},
+	{413, "Request Entity Too Large"},
+	{414, "Request-URI Too Large"},
+	{415, "Unsupported Media Type"},
+	{416, "Requested range not satisfiable"},
+	{417, "Expectation Failed"},
+
+	{500, "Internal Server Error"},
+	{501, "Not Implemented"},
+	{502, "Bad Gateway"},
+	{503, "Service Unavailable"},
+	{504, "Gateway Time-out"},
+	{505, "HTTP Version not supported"},
+	NULL,
+};
+
 const char *t_Methods[]=
 {
 	"GET",
